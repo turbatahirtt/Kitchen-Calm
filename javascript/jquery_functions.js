e95f1548ccae8a969a49df8137624d53421b4b66
turbatahirtt <160163736+turbatahirtt@users.noreply.github.com>
2024-03-05 00:35:40 -0500

    Add folders

---
+++ javascript/jquery_functions.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+
+  var homeButtonId = "#home-button";
+  var tipsButtonId = "#tips-button";
+  var contactButtonId = "#contact-button";
+
+  var homeDivId = "#home";
+  var tipsDivId = "#tips";
+  var contactDivId = "#contact";
+
+})
